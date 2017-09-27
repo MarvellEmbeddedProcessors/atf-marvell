504dbfddc9a01780eabd2ebaca8a823e1859aada
yachun <yachun@marvell.com>
2017-09-27 16:35:08 +0800

    board: a3900: DDR speed set to 2133R
    AXIS board is more stable when DDR speed set to 2133R.
    
    Change-Id: Ie51b4684dba57edfbde9b61ab02289e6de384066
    Signed-off-by: yachun <yachun@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/48561
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Kostya Porotchkin <kostap@marvell.com>

--- plat/marvell/a8k/a3900/board/dram_port.c
+++ plat/marvell/a8k/a3900/board/dram_port.c
@@ -76,7 +76,7 @@ static struct mv_ddr_topology_map board_topology_map = {
 	      {0x1, 0x0, 0, 0},
 	      {0x1, 0x0, 0, 0},
 	      {0x1, 0x0, 0, 0} },
-	   SPEED_BIN_DDR_2400P,		/* speed_bin */
+	   SPEED_BIN_DDR_2133R,		/* speed_bin */
 	   MV_DDR_DEV_WIDTH_16BIT,	/* sdram device width */
 	   MV_DDR_DIE_CAP_8GBIT,	/* die capacity */
 	   MV_DDR_FREQ_SAR,		/* frequency */
