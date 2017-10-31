8ed5861c4abcdefc11fd5953f7127167ce976095
Hanna Hawa <hannah@marvell.com>
2017-10-31 14:41:29 +0200

    ap810: remove the defined north bridge count in board platform
    Change-Id: I0f46bd86a33225a76635b431101bb2157bd42a40
    Signed-off-by: Hanna Hawa <hannah@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/45724
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Kostya Porotchkin <kostap@marvell.com>

--- plat/marvell/a8k-p/a8xx0/plat_def.h
+++ plat/marvell/a8k-p/a8xx0/plat_def.h
@@ -10,12 +10,4 @@
 
 #include <a8kp_plat_def.h>
 
-/* For now we'll have North bridge count to
-** configure the number of APs, later we'll
-** add function to read AP count */
-#if PLAT_MARVELL_NORTHB_COUNT
-#undef PLAT_MARVELL_NORTHB_COUNT
-#define PLAT_MARVELL_NORTHB_COUNT	1
-#endif
-
 #endif /* __MVEBU_DEF_H__ */
