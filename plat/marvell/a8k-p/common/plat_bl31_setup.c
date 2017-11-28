6b55e9b94eb7177fb8b36eb855831e5f50dd3d9b
Hanna Hawa <hannah@marvell.com>
2017-11-28 13:59:55 +0200

    bl31: ap810: remove unused code from bl31 plat arch function
    Change-Id: I116e5f8c06fc8a3b6a16d1784c7d3b01a74e3857
    Signed-off-by: Hanna Hawa <hannah@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/46989
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Kostya Porotchkin <kostap@marvell.com>

--- plat/marvell/a8k-p/common/plat_bl31_setup.c
+++ plat/marvell/a8k-p/common/plat_bl31_setup.c
@@ -9,42 +9,9 @@
 #include <plat_config.h>
 #include <plat_private.h>
 #include <ap810_setup.h>
-#include <cp110_setup.h>
 #include <marvell_pm.h>
 #include <mmio.h>
 #include <debug.h>
-
-#ifdef SCP_IMAGE
-#include <mss_ipc_drv.h>
-#include <mss_mem.h>
-#endif
-
-/* Set a weak stub for platforms that don't need to configure GPIO */
-#pragma weak marvell_gpio_config
-int marvell_gpio_config(void)
-{
-	return 0;
-}
-
-void marvell_bl31_mpp_init(void)
-{
-	return;
-}
-
-void marvell_bl31_mss_init(void)
-{
-#ifdef SCP_IMAGE
-	struct mss_pm_ctrl_block *mss_pm_crtl =
-			(struct mss_pm_ctrl_block *)MSS_SRAM_PM_CONTROL_BASE;
-
-	INFO("MSS IPC init\n");
-
-	if (mss_pm_crtl->ipc_state == IPC_INITIALIZED)
-		mv_pm_ipc_init(mss_pm_crtl->ipc_base_address | MVEBU_REGS_BASE);
-#else
-	INFO("MSS is not supported in this build\n");
-#endif
-}
 
 /* This function overruns the same function in marvell_bl31_setup.c */
 void bl31_plat_arch_setup(void)
@@ -65,18 +32,4 @@ void bl31_plat_arch_setup(void)
 	if (mailbox[MBOX_IDX_MAGIC] != MVEBU_MAILBOX_MAGIC_NUM ||
 	    mailbox[MBOX_IDX_SUSPEND_MAGIC] != MVEBU_MAILBOX_SUSPEND_STATE)
 		marvell_bl31_plat_arch_setup();
-
-	/* Configure CP110 */
-	INFO("Call CP110 init function\n");
-
-	/* Should be called only after setting IOB windows */
-	marvell_bl31_mpp_init();
-
-	/* initialize IPC between MSS and ATF */
-	if (mailbox[MBOX_IDX_MAGIC] != MVEBU_MAILBOX_MAGIC_NUM ||
-	    mailbox[MBOX_IDX_SUSPEND_MAGIC] != MVEBU_MAILBOX_SUSPEND_STATE)
-		marvell_bl31_mss_init();
-
-	/* Configure GPIO */
-	marvell_gpio_config();
 }
