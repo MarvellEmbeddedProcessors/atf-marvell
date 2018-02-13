b634f9b1a4e16a10aa10ee92d0aa0bc33cece913
Konstantin Porotchkin <kostap@marvell.com>
2018-02-13 14:52:10 +0200

    a8xxy: Extend the DRAM configuration to cover 4 memory slots
    Extend the DRAM port to include the basic topology details for
    maximum supported memory slots - 2 on each AP.
    
    Change-Id: Icb44a4bea6c403b4216e5450d5c9fd9a28afd99e
    Signed-off-by: Konstantin Porotchkin <kostap@marvell.com>
    Reviewed-on: http://vgitil04.il.marvell.com:8080/50516
    Tested-by: iSoC Platform CI <ykjenk@marvell.com>
    Reviewed-by: Hanna Hawa <hannah@marvell.com>

--- plat/marvell/a8k-p/a8xxy/board/dram_port.c
+++ plat/marvell/a8k-p/a8xxy/board/dram_port.c
@@ -65,7 +65,36 @@ struct mv_ddr_iface dram_iface_ap0[DDR_MAX_UNIT_PER_AP] = {
 		},
 	},
 	{
-		.state = MV_DDR_IFACE_DNE,
+		.state = MV_DDR_IFACE_NRDY,
+		.id = 1,
+		.spd_data_addr = I2C_SPD_DATA_ADDR(0, 1),
+		.spd_page_sel_addr = I2C_SPD_P0_SEL_ADDR,
+		.tm = {
+			/* MISL board with 1CS 8Gb x4 devices of Micron 2400T */
+			DEBUG_LEVEL_ERROR,
+			0x1, /* active interfaces */
+			/* cs_mask, mirror, dqs_swap, ck_swap X subphys */
+			{ { { {0x1, 0x0, 0, 0},	/* FIXME: change the cs mask for all 64 bit */
+				    {0x1, 0x0, 0, 0},
+				    {0x1, 0x0, 0, 0},
+				    {0x1, 0x0, 0, 0},
+				    {0x1, 0x0, 0, 0},
+				    {0x1, 0x0, 0, 0},
+				    {0x1, 0x0, 0, 0},
+				    {0x1, 0x0, 0, 0},
+				    {0x1, 0x0, 0, 0} },
+			/* TODO: double check if the speed bin is 2400T */
+			SPEED_BIN_DDR_2400T,		/* speed_bin */
+			MV_DDR_DEV_WIDTH_8BIT,	/* sdram device width */
+			MV_DDR_DIE_CAP_8GBIT,	/* die capacity */
+			MV_DDR_FREQ_SAR,		/* frequency */
+			0, 0,			/* cas_l, cas_wl */
+			MV_DDR_TEMP_LOW} },		/* temperature */
+			MV_DDR_64BIT_ECC_PUP8_BUS_MASK, /* subphys mask */
+			MV_DDR_CFG_SPD,			/* ddr configuration data source */
+			{ {0} },			/* raw spd data */
+			{0}				/* timing parameters */
+		},
 	},
 };
 struct mv_ddr_iface dram_iface_ap1[DDR_MAX_UNIT_PER_AP] = {
@@ -102,7 +131,36 @@ struct mv_ddr_iface dram_iface_ap1[DDR_MAX_UNIT_PER_AP] = {
 		},
 	},
 	{
-		.state = MV_DDR_IFACE_DNE,
+		.state = MV_DDR_IFACE_NRDY,
+		.id = 1,
+		.spd_data_addr = I2C_SPD_DATA_ADDR(1, 1),
+		.spd_page_sel_addr = I2C_SPD_P0_SEL_ADDR,
+		.tm = {
+			/* MISL board with 1CS 8Gb x4 devices of Micron 2400T */
+			DEBUG_LEVEL_ERROR,
+			0x1, /* active interfaces */
+			/* cs_mask, mirror, dqs_swap, ck_swap X subphys */
+			{ { { {0x1, 0x0, 0, 0},	/* FIXME: change the cs mask for all 64 bit */
+				    {0x1, 0x0, 0, 0},
+				    {0x1, 0x0, 0, 0},
+				    {0x1, 0x0, 0, 0},
+				    {0x1, 0x0, 0, 0},
+				    {0x1, 0x0, 0, 0},
+				    {0x1, 0x0, 0, 0},
+				    {0x1, 0x0, 0, 0},
+				    {0x1, 0x0, 0, 0} },
+			/* TODO: double check if the speed bin is 2400T */
+			SPEED_BIN_DDR_2400T,		/* speed_bin */
+			MV_DDR_DEV_WIDTH_8BIT,	/* sdram device width */
+			MV_DDR_DIE_CAP_8GBIT,	/* die capacity */
+			MV_DDR_FREQ_SAR,		/* frequency */
+			0, 0,			/* cas_l, cas_wl */
+			MV_DDR_TEMP_LOW} },		/* temperature */
+			MV_DDR_64BIT_ECC_PUP8_BUS_MASK, /* subphys mask */
+			MV_DDR_CFG_SPD,			/* ddr configuration data source */
+			{ {0} },			/* raw spd data */
+			{0}				/* timing parameters */
+		},
 	},
 };
 
